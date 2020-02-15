7739891f882f70b8f2e9ac631513acee5b894255
git <nickshelenin@gmail.com>
2020-02-15 14:17:24 +0600

    intial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { BrowserRouter as Router, Redirect, Switch, Route } from 'react-router-dom';
+
+import Home from '../Home/Home';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import PortfolioPage from './pages/PortfolioPage';
+import ContactPage from './pages/ContactPage';
+import Project from './pages/Project';
+import NotFoundPage from './pages/NotFoundPage';
+
+class App extends React.Component {
+   state = {
+      project1: {
+         website: 'proclean',
+         title: 'proClean is a cleaning company website',
+         img: 'screenshot.png',
+         about:
+            'This is a informational website for fake company ProClean. All technologies that have been used in this project listed below.   ',
+         techSheet: ['HTML5', 'CSS3', 'Scss', 'JavaScript'],
+         websiteLink: 'https://nickshelenin.github.io/proClean/',
+         codeLink: 'https://github.com/nickshelenin/proClean'
+      }
+   };
+
+   render() {
+      return (
+         <Router>
+            <div className="page">
+               <Navbar />
+
+               <Switch>
+                  <Route exact path="/" component={HomePage} />
+                  <Route exact path="/about" component={AboutPage} />
+                  <Route exact path="/portfolio" component={PortfolioPage} />
+                  <Route exact path="/contact" component={ContactPage} />
+
+                  <Route exact path="/portfolio/1">
+                     <Project data={this.state.project1} />
+                  </Route>
+
+                  <Route to="*" component={NotFoundPage} />
+               </Switch>
+            </div>
+         </Router>
+      );
+   }
+}
+
+export default App;
