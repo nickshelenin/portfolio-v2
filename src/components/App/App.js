87bde95f936f45cf08dbd70697a8435375d1b8f4
nick <nickshelenin@gmail.com>
2020-03-06 00:13:43 +0600

    project 2 added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,11 +14,21 @@ class App extends React.Component {
       project1: {
          website: "proclean",
          title: "proClean is a cleaning company website",
-         img: "screenshot.png",
-         about: "This is an informational website for fake company ProClean. All technologies that have been used in this project listed below.   ",
-         techSheet: ["HTML5", "CSS3", "Scss", "JavaScript"],
+         img: "project1.png",
+         about: "This is an informational website for fake company ProClean. All technologies that have been used in this project listed below.",
+         techSheet: ["HTML", "CSS", "Scss", "JavaScript"],
          websiteLink: "https://nickshelenin.github.io/proClean/",
          codeLink: "https://github.com/nickshelenin/proClean"
+      },
+      project2: {
+         website: "moviefan",
+         title: "moviefan - movie searching app based on React",
+         img: "project2.png",
+         about:
+            "This is a movie searching app that helps you find information on any movie or tv show including info about cast. The app is built with React and TMDB api for fetching data about movies.",
+         techSheet: ["React", "Scss", "TMDB api"],
+         websiteLink: "https://moviefan.netlify.com",
+         codeLink: "https://github.com/nickshelenin/moviefan"
       }
    };
 
@@ -37,7 +47,11 @@ class App extends React.Component {
                      <Project data={this.state.project1} />
                   </Route>
 
-                  <Route to='*' component={NotFoundPage} />
+                  <Route exact path='/portfolio/2'>
+                     <Project data={this.state.project2} />
+                  </Route>
+
+                  {/* <Route component={NotFoundPage} /> */}
                </Switch>
             </Router>
          </>
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -2,41 +2,27 @@ import React, { Component } from "react";
 import { Link, BrowserRouter as Router } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 
-import './Portfolio.scss'
+import "./Portfolio.scss";
 
 class PortfolioPage extends Component {
    render() {
       return (
-         <div className="page portfolio-page">
-            <div className="page-title-container">
-               <h1 className="page-title">web developer portfolio</h1>
-               <p>
-                 Check out my latest web development projects.
-               </p>
+         <div className='page portfolio-page'>
+            <div className='page-title-container'>
+               <h1 className='page-title'>web developer portfolio</h1>
+               <p>Check out my latest web development projects.</p>
             </div>
 
-            <div className="projects">
-               <Link to="/portfolio/1">
-                  <div className="project-card">
-                     <img src="/img/screenshot.png" alt="" />
+            <div className='projects'>
+               <Link to='/portfolio/1'>
+                  <div className='project-card'>
+                     <img src='/img/project1.png' alt='' />
                   </div>
                </Link>
 
-               <Link to="/portfolio/2">
-                  <div className="project-card">
-                     <img src="/img/screenshot.png" alt="" />
-                  </div>
-               </Link>
-
-               <Link to="/portfolio/3">
-                  <div className="project-card">
-                     <img src="/img/screenshot.png" alt="" />
-                  </div>
-               </Link>
-
-               <Link to="/portfolio/4">
-                  <div className="project-card">
-                     <img src="/img/screenshot.png" alt="" />
+               <Link to='/portfolio/2'>
+                  <div className='project-card'>
+                     <img src='/img/project2.png' alt='' />
                   </div>
                </Link>
             </div>
